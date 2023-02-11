51dcd1648b551b3d5f36cdbae411b3c21bca8f6d
mingi0416 <mingi0416@gmail.com>
2023-02-11 18:46:03 +0900

    테스트 공부

---
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,78 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest // 해당 클래스는 스프링부트와 연동되어 테스팅된다
+class ArticleServiceTest {
+
+    @Autowired ArticleService articleService;
+
+    @Test
+    void index() {
+        // 예상
+        Article a = new Article(1L, "가가가가", "1111");
+        Article b = new Article(2L, "나나나나", "2222");
+        Article c = new Article(3L, "다다다다", "3333");
+        Article d = new Article(4L, "깃업로드", "테스트");
+        List<Article> expected = new ArrayList<Article>(Arrays.asList(a, b, c, d));
+        // 실제
+        List<Article> articles = articleService.index();
+
+        // 비교
+        assertEquals(expected.toString(), articles.toString());
+    }
+
+    @Test
+    void show_성공____존재하는_id_입력() {
+        // 예상
+        Long id = 1L;
+        Article expected = new Article(id, "가가가1가", "1111");
+        // 실패
+        Article article = articleService.show(id);
+        // 비교
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    void show_실패____존재하지_않는_id_입력() {
+        // 예상
+        Long id = -1L;
+        Article expected = null;
+        // 실패
+        Article article = articleService.show(id);
+        // 비교
+        assertEquals(expected, article);
+    }
+
+    @Test
+    void create_성공_title과_content만_있는_dto_입력() {
+        // 예상
+        String title = "라라라라";
+        String content = "5555";
+        ArticleForm dto = new ArticleForm(null, title, content);
+        Article expected = new Article(5L, title, content);
+        // 실제
+        Article article = articleService.create(dto);
+        // 비교
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    void create_실패() {
+        //예상
+
+        //실패
+
+        //비교
+    }
+}
